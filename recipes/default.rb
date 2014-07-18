68fafd42e832a40890bfa0e7030c8665f82aadd5
Nik Ormseth <nik@ormseth.net>
2014-07-17 18:36:27 -0700

    refactored for LWRP within sites loop

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,40 +26,15 @@ execute "mv #{welcome_file} /etc/httpd/conf.d/welcome.conf.disabled" do
   notifies :restart, "service[httpd]"
 end
 
-# Use the LWRP we've created for a new site
-apache_vhost "lions" do
-  site_port 8080
-  action :create
-  notifies :restart, "service[httpd]"
-end
-
 # Iterate over the apache sites
 node["apache"]["sites"].each do |site_name, site_data|
-  # set document root
-  document_root = "/srv/apache#{site_name}"
   
-  # add template for virtual host config
-  template "/etc/httpd/conf.d/#{site_name}.conf" do
-    source "custom.erb"
-    mode "0644"
-    variables(
-      :document_root => document_root,
-      :port => site_data["port"]      
-     )
-     notifies :restart, "service[httpd]"
+  # Use the LWRP we've created for a new site
+  apache_vhost "#{site_name}" do
+    site_port site_data['port']
+    action :create
+    notifies :restart, "service[httpd]"
   end
-  
-  # Add a dir resource to create the document_root
-  directory document_root do
-    mode "0755"
-    recursive true
-  end
-  
-  # Add a template resource for the virtual host's index.html
-  template "#{document_root}/index.html" do
-    source "index.html.erb"
-    mode "0644"
-    variables(:site_name => site_name, :port => site_data["port"])
-  end
+
 end
 
